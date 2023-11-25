2206b582b6d46694c0083f429bdc270e4b11bb25
MR Masum <mrmasum820@gmail.com>
2023-11-26 02:21:54 +0600

    ready homepage with router

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useLoaderData } from 'react-router-dom'
+import './App.css'
+import CoffeeCard from './components/CoffeeCard'
+import { useState } from 'react'
+
+function App() {
+  const loadedCoffees = useLoaderData()
+  const [coffees, setCoffees] = useState(loadedCoffees)
+
+  return (
+    <div className='m-20'>
+      <h1 className='text-5xl text-purple-400 text-center my-8'>Our popular coffees: {coffees.length}</h1>
+      <div className='grid md:grid-cols-2 gap-4'>
+        {
+          coffees.map(coffee => <CoffeeCard
+            key={coffee._id}
+            coffee={coffee}
+            coffees={coffees}
+            setCoffees={setCoffees}
+          ></CoffeeCard>)
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
+
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
+    const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your coffee has been deleted.",
+                                icon: "success"
+                            });
+                            const remaining = coffees.filter(coff => coff._id !== _id)
+                            setCoffees(remaining)
+                        }
+                    })
+            }
+        });
+    }
+
+    return (
+        <div className="card card-side bg-base-100 shadow-xl">
+            <figure><img src={photo} alt="photos" /></figure>
+            <div className="flex justify-between w-full p-4">
+                <div>
+                    <h2 className="card-title">{name}</h2>
+                    <p>{quantity}</p>
+                    <p>{supplier}</p>
+                    <p>{taste}</p>
+                </div>
+
+                <div className="card-actions justify-end">
+                    <div className="btn-group btn-group-vertical space-y-4">
+                        <button className="btn">view</button>
+                        <Link to={`/updateCoffee/${_id}`}>
+                            <button className="btn">edit</button>
+                        </Link>
+                        <button onClick={() => handleDelete(_id)} className="btn bg-orange-400">X</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CoffeeCard;
